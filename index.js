4586de6d95331a199785f513b889ffe29833c23a
purerosefallen <78877@qq.com>
2019-06-02 15:40:57 +0800

    log

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const port = parseInt(process.argv[2]);
 http.createServer((request, response) => {
 	fs.readdir(path, (err, files) => { 
 		if (err) { 
-			response.writeHead(400);
-			response.end("Read error.");
+			const status_code = 400;
+			const ret = "Read error.";
+			console.log(request.connection.remoteAddress, status_code, ret);
+			response.writeHead(status_code);
+			response.end(ret);
 			return;
 		}
 		var found = 0, found_full;
@@ -21,16 +24,23 @@ http.createServer((request, response) => {
 			}
 		}
 		if (!found) { 
-			response.writeHead(404);
-			response.end("Not found.");
+			const status_code = 404;
+			const ret = "Not found.";
+			console.log(request.connection.remoteAddress, status_code, ret);
+			response.writeHead(status_code);
+			response.end(ret);
 			return;
 		}
 		fs.readFile(path + "/" + found_full, (error, buffer) => { 
 			if (error) { 
-				response.writeHead(400);
-				response.end("Invalid file.");
+				const status_code = 400;
+				const ret = "Invalid file.";
+				console.log(request.connection.remoteAddress, status_code, ret, path + "/" + found_full);
+				response.writeHead(status_code);
+				response.end(ret);
 				return;
 			}
+			console.log(request.connection.remoteAddress, 200, path + "/" + found_full);
 			response.writeHead(200, { "Content-Type": process.argv[4] || "application/octet-stream" });
 			response.end(buffer);
 		});
